67227e649603298b3792c1bc014ea3841deef142
zakkiislamii <mzislami05@gmail.com>
2025-10-09 21:36:32 +0700

    tes1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NotificationsController } from './notifications/notifications.controller';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [NotificationsModule],
+  controllers: [AppController, NotificationsController],
   providers: [AppService],
 })
 export class AppModule {}
